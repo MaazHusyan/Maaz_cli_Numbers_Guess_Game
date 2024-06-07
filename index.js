d855446143484687697f4739db4c7a95e7cce4f5
Maaz Husyan <159529283+MaazHusyan@users.noreply.github.com>
2024-06-07 20:18:22 +0500

    Edited

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
 let randomNum = Math.floor(Math.random() * 5 + 1);
-const answers = await inquirer.prompt([{
+const answers = await inquirer_1.default.prompt([{
         name: "userGuessedNum",
         type: "number",
         message: "Guess One Digit Number Between 1 to 6"
